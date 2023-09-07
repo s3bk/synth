519ad819174db999e6993b48350791f59ddf7778
Sebastian K <s3bk@protonmail.com>
2023-09-08 00:28:53 +0200

    bump

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -1,13 +1,49 @@
-use std::{path::Path, time::Duration};
+use std::path::Path;
 
 use cpal::{traits::{HostTrait, DeviceTrait, StreamTrait}, SampleFormat};
 
 use synth::midi::parse_midi;
-pub use synth::synth::Synth;
+pub use synth::synth::{Synth, Profile};
 
 fn main() {
+    let profiles = vec![
+        // 0
+        Profile::Simple { attack: 1.0, falloff: 1.0, colors: &[1.0, 0.8, 0.5, 0.2, 0.1]},
+        
+        // 1: clar1
+        Profile::Simple { attack: 20.0, falloff: 40.0, colors: &[0.6, 1.0, 0.8, 0.5, 0.2]},
+
+        // 2: bass2
+        Profile::Simple { attack: 20.0, falloff: 20.0, colors: &[1.0, 0.6, 0.3, 0.6, 0.2]},
+
+        // 3: syn
+        Profile::Simple { attack: 10., falloff: 40.0, colors: &[1.0, 0.8, 0.3, 0.6, 0.2]},
+
+        // 4: piano
+        Profile::Simple { attack: 1.0, falloff: 40.0, colors: &[1.0, 0.7, 0.5, 0.4, 0.2]},
+        
+        // 5: clar1
+        Profile::Simple { attack: 10.0, falloff: 40.0, colors: &[0.6, 1.0, 0.8, 0.5, 0.2]},
+
+        // 6: drum
+        Profile::Drum { falloff: 10.0, colors: &[1.0, 0.4, 0.8, 0.5, 0.2]},
+
+        // 7: drum
+        Profile::Drum { falloff: 10.0, colors: &[2.0, 0.8, 0.5, 0.2]},
+
+        // 8: snare
+        Profile::Drum { falloff: 10.0, colors: &[0.6, 1.0, 0.8, 0.7, 0.5, 0.4, 0.2]},
+
+        // 9: hihat
+        Profile::Drum { falloff: 3.0, colors: &[0.6, 0.4, 1.0, 0.5, 0.2]},
+    ];
+
     let mut notes = parse_midi(Path::new("Crazy_Frog_Axel.mid"));
-    //notes.retain(|n| n.track == 1);
+
+    if let Some(t) = std::env::args().nth(1) {
+        let t = t.parse().unwrap();
+        notes.retain(|n| n.track == t);
+    }
 
     let host = cpal::default_host();
     
@@ -21,7 +57,7 @@ fn main() {
         .next().unwrap()
         .with_max_sample_rate();
 
-    let mut synth = Synth::new(config.sample_rate().0, config.channels());
+    let (mut synth, handle) = Synth::new(config.sample_rate().0, config.channels(), profiles);
     synth.queue(notes);
 
     let stream = device.build_output_stream(&config.into(),
@@ -32,5 +68,5 @@ fn main() {
     
     stream.play().unwrap();
 
-    std::thread::sleep(Duration::from_secs(60));
+    handle.wait();
 }
--- src/synth.rs
+++ src/synth.rs
@@ -1,12 +1,20 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, sync::{Condvar, Arc, mpsc::{Receiver, channel, Sender}}};
 
 use crate::Note;
 
-static COLORS: &[&[f32]] = &[
-    &[1.0, 0.8, 0.5, 0.2, 0.1],
-    &[0.6, 1.0, 0.8, 0.5, 0.2],
-    &[1.0, 0.2, 0.3, 0.6, 0.2],
-];
+
+enum Msg {
+    Done
+}
+
+pub struct SynthHandle {
+    rx: Receiver<Msg>,
+}
+impl SynthHandle {
+    pub fn wait(&self) {
+        self.rx.recv().unwrap();
+    }
+}
 
 pub struct Synth {
     channels: u16,
@@ -15,24 +23,32 @@ pub struct Synth {
     playing: Vec<Voice>,
     time: f64,
     sample_duration: f64,
+    tx: Sender<Msg>,
+    profiles: Vec<Profile>,
 }
 impl Synth {
-    pub fn new(sample_rate: u32, channels: u16) -> Self {
-        Synth {
+    pub fn new(sample_rate: u32, channels: u16, profiles: Vec<Profile>) -> (Self, SynthHandle) {
+        let (tx, rx) = channel();
+
+        let synth = Synth {
             channels,
             sample_rate,
             queue: Default::default(),
             playing: Default::default(),
             time: 0.0,
             sample_duration: 1.0 / sample_rate as f64,
-        }
+            tx,
+            profiles
+        };
+        let handle = SynthHandle { rx };
+        (synth, handle)
     }
     fn process_part(&mut self, output: &mut [f32]) {
         output.fill(0.0);
         if self.playing.len() == 0 {
             return;
         }
-        println!("{} voices for {} samples", self.playing.len(), output.len() / self.channels as usize);
+        // println!("{} voices for {} samples", self.playing.len(), output.len() / self.channels as usize);
         for voice in self.playing.iter_mut() {
             match voice {
                 Voice::Simple(v) => {
@@ -42,10 +58,18 @@ impl Synth {
                         }
                     }
                 }
+                Voice::Drum(v) => {
+                    for out in output.chunks_exact_mut(self.channels as usize) {
+                        for c in out.iter_mut() {
+                            *c += v.sample();
+                        }
+                    }
+                }
             }
         }
         self.playing.retain(|v| match v {
-            Voice::Simple(v) => !v.stopped()
+            Voice::Simple(v) => !v.stopped(),
+            Voice::Drum(v) => !v.stopped(),
         });
     }
     pub fn process(&mut self, mut remaining: &mut [f32]) {
@@ -56,9 +80,9 @@ impl Synth {
             //dbg!(next);
             if next.time < end_time {
                 let note = self.queue.pop_front().unwrap();
-                let color = COLORS[note.track % COLORS.len()];
-                let v = SimpleVoice::new(note.freq, self.sample_duration, 0.1, note.duration, color);
-                self.playing.push(Voice::Simple(v));
+                let profile = &self.profiles[note.track % self.profiles.len()];
+                let v = profile.voice(note.freq, self.sample_duration, 0.1, note.duration);
+                self.playing.push(v);
 
                 let samples = ((note.time - self.time) * self.sample_rate as f64) as usize;
                 let idx = samples * self.channels as usize;
@@ -76,15 +100,35 @@ impl Synth {
             self.process_part(remaining);
             self.time += (remaining.len() / self.channels as usize) as f64 * self.sample_duration;
         }
+        if self.queue.len() == 0 {
+            self.tx.send(Msg::Done).unwrap();
+        }
     }
     pub fn queue(&mut self, mut notes: Vec<Note>) {
         notes.sort_by(|a, b| a.time.partial_cmp(&b.time).unwrap());
         self.queue.extend(notes);
+        if let Some(first) = self.queue.front() {
+            self.time = first.time;
+        }
     }
 }
 
 enum Voice {
-    Simple(SimpleVoice)
+    Simple(SimpleVoice),
+    Drum(DrumVoice),
+}
+
+pub enum Profile {
+    Simple { colors: &'static [f32], attack: f32, falloff: f32 },
+    Drum { colors: &'static [f32], falloff: f32 },
+}
+impl Profile {
+    fn voice(&self, freq: f64, sample_duration: f64, amplitude: f64, duration: f64) -> Voice {
+        match *self {
+            Profile::Simple { colors, attack, falloff } => Voice::Simple(SimpleVoice::new(freq, sample_duration, amplitude, duration, attack, falloff, colors)),
+            Profile::Drum { colors, falloff } => Voice::Drum(DrumVoice::new(freq, sample_duration, amplitude, duration, falloff, colors))
+        }
+    }
 }
 
 struct SimpleVoice {
@@ -93,16 +137,21 @@ struct SimpleVoice {
     amplitude: f32,
     target_amplitide: f32,
     samples_remaining: i64,
+    attack: f32,
+    falloff: f32,
     colors: &'static [f32],
 }
 impl SimpleVoice {
-    pub fn new(freq: f64, sample_duration: f64, amplitude: f64, duration: f64, colors: &'static [f32]) -> Self {
+    pub fn new(freq: f64, sample_duration: f64, amplitude: f64, duration: f64, attack: f32, falloff: f32, colors: &'static [f32]) -> Self {
+        let phase_step = (sample_duration * freq) as f32;
         SimpleVoice {
             phase: 0.0,
-            phase_step: (sample_duration * freq) as f32,
-            amplitude: amplitude as f32,
-            target_amplitide: amplitude as f32,
-            samples_remaining: (duration / sample_duration) as i64,
+            phase_step,
+            amplitude: 0.0,
+            target_amplitide: (100. * amplitude as f32 / freq as f32).min(0.5),
+            samples_remaining: ((duration + 3.0 * falloff as f64 / freq) / sample_duration) as i64,
+            falloff: (phase_step / falloff).min(1.0),
+            attack: (phase_step / attack).min(1.0),
             colors,
         }
     }
@@ -116,7 +165,12 @@ impl SimpleVoice {
             self.stop();
         }
 
-        self.amplitude = 0.02 * self.target_amplitide + 0.98 * self.amplitude;
+        let f = if self.samples_remaining > 0 {
+            self.attack
+        } else {
+            self.falloff
+        };
+        self.amplitude = f * self.target_amplitide + (1.0 - f) * self.amplitude;
         self.phase = (self.phase + self.phase_step).fract();
 
         self.samples_remaining -= 1;
@@ -126,6 +180,44 @@ impl SimpleVoice {
         ).sum::<f32>()* self.amplitude 
     }
     pub fn stopped(&self) -> bool {
-        self.samples_remaining <= 0 && self.amplitude < 0.001
+        self.samples_remaining <= 0
+    }
+}
+struct DrumVoice {
+    phase: f32,
+    phase_step: f32,
+    amplitude: f32,
+    falloff: f32,
+    colors: &'static [f32],
+    samples_remaining: i64,
+}
+impl DrumVoice {
+    pub fn new(freq: f64, sample_duration: f64, amplitude: f64, duration: f64, falloff: f32, colors: &'static [f32]) -> Self {
+        let phase_step = (sample_duration * freq) as f32;
+        DrumVoice {
+            phase: 0.0,
+            phase_step,
+            amplitude: (100. * amplitude as f32 / freq as f32).min(0.5),
+            falloff: (phase_step / falloff).min(1.0),
+            samples_remaining: ((falloff as f64 / freq * 5.0) / sample_duration) as i64,
+            colors,
+        }
+    }
+    pub fn sample(&mut self) -> f32 {
+        use std::f32::consts::PI;
+
+
+        let f = self.falloff;
+        self.amplitude = (1.0 - f) * self.amplitude;
+        self.phase = (self.phase + self.phase_step).fract();
+
+        self.samples_remaining -= 1;
+
+        self.colors.iter().enumerate().map(|(i, &c)| 
+            (i as f32 * 2.0 * PI * self.phase as f32).sin() * c
+        ).sum::<f32>()* self.amplitude 
+    }
+    pub fn stopped(&self) -> bool {
+        self.samples_remaining <= 0
     }
 }
